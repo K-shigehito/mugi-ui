c33a63638a0ab6e695790f12159544f7e5466399
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2021-07-24 00:39:45 +0900

    dialogコンポーネントの作成

---
+++ src/components/modals/DialogSample.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <p class="mb-2 text-sxl">Dialogs</p>
+    <div>
+      <MyDialog :visible="dialogVisible" @backdrop-clicked="switchDialog">
+        <template #title>
+          <p class="font-bold">ダイアログ</p>
+        </template>
+        <template #body>
+          ダミー本文です。ダミー本文です。ダミー本文です。ダミー本文です。ダミー本文です。ダミー本文です。ダミー本文です。ダミー本文です。ダミー本文です。
+        </template>
+        <template #footer>
+          <div class="flex justify-end">
+            <SecondaryButton @click="switchDialog">close</SecondaryButton>
+          </div>
+        </template>
+      </MyDialog>
+    </div>
+    <div class="mb-2">
+      <PrimaryButton @click="switchDialog">open</PrimaryButton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import PrimaryButton from '../buttons/PrimaryButton.vue';
+import SecondaryButton from '../buttons/SecondaryButton.vue';
+import MyDialog from './MyDialog.vue';
+
+export default defineComponent({
+  name: 'DialogSample',
+  components: {
+    PrimaryButton,
+    SecondaryButton,
+    MyDialog,
+  },
+  setup() {
+    const dialogVisible = ref(false);
+    const switchDialog = () => {
+      dialogVisible.value = !dialogVisible.value;
+    };
+
+    return {
+      dialogVisible,
+      switchDialog,
+    };
+  },
+});
+</script>
