b688201faf075c7c03e425c0f3ed33d059e14322
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2021-07-25 14:53:55 +0900

    コンテンツローダーの設置

---
+++ src/components/loader/ContentsLoaderSample.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p class="mb-2 text-2xl">ContentsLoade</p>
+    <ContentsLoader :line="2" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ContentsLoader from './ContentsLoader.vue';
+
+export default defineComponent({
+  name: 'ContentsLoaderSample',
+  components: {
+    ContentsLoader,
+  },
+});
+</script>
