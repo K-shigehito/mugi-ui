b655516b23bcac15a74f75dd843ae902b6415bfa
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2021-07-26 16:20:01 +0900

    ライブラリのビルド設定

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import ButtonSample from './components/buttons/ButtonSample.vue';
-import DialogSample from './components/modals/DialogSample.vue';
+import DialogSample from './components/dialog/DialogSample.vue';
 import InputTextSample from './components/forms/InputTextSample.vue';
 import InputRadioSample from './components/forms/InputRadioSample.vue';
 import ContentsLoaderSample from './components/loader/ContentsLoaderSample.vue';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build:
+    process.env.NODE_ENV === 'development'
+      ? {}
+      : {
+          lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'mugi-ui',
+          },
+          rollupOptions: {
+            external: ['vue'],
+            output: {
+              globals: {
+                vue: 'Vue',
+              },
+            },
+          },
+        },
 });
